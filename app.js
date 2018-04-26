ed1058968d8f14e7db7c6c187432aa53dac5017a
josefebrian <shirleyarianto24@gmail.com>
2018-04-26 14:14:41 +0700

    masih berantakan

--- app.js
+++ app.js
@@ -1,32 +1,113 @@
+const electron = require('electron')
 var express = require('express');
+var storage = require('electron-json-storage')
 var exp = express();
+// Module to control application life.
+// Module to create native browser window.
+// Module to create menu
+
+const{app, BrowserWindow, ipcMain, Menu,menuItem} = electron;
+
+const path = require('path')
+const url = require('url')
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
+
+function createWindow () {
+
+  // var request = require('request');
+  //   request('http://localhost:3000/user', function (error, response, body) {
+  //       console.log('error:', error); // Print the error if one occurred
+  //       console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  //       console.log('body:', body); // Print the HTML for the Google homepage.
+  // });
+  // Create the browser window.
+  mainWindow = new BrowserWindow({width: 700, height: 500})
+
+  // and load the index.html of the app.
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+
+  }))
+
+  mainWindow.on('closed', function(){
+    app.quit();
+  })
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools();
+
+  // Build menu from template
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  // Insert menu
+  Menu.setApplicationMenu(mainMenu);
+
+// // context menu
+// require('electron-context-menu')({
+//   prepend: (params, browserWindow) => [{
+//     labels: {
+//   cut: 'Configured Cut',
+//   copy: 'Configured Copy',
+//   paste: 'Configured Paste',
+//   save: 'Configured Save Image',
+//   copyLink: 'Configured Copy Link'
+//     },
+//     // Only show it when right-clicking images
+//     visible: params.mediaType === 'image'
+//   }]
+// });
+
+// make right click like a website
+// const ctxMenu = new Menu();
+//   ctxMenu.append(new MenuItem({
+//     label: 'test'
+//   }))
+
+//   mainWindow.webContents.on('context-menu',function(e,params){
+//     ctxMenu.popup(mainWindow, params.x, params.y)
+//   })
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
 
 exp.use(express.static('public'))
 
 var mysql      = require('mysql');
 var db = mysql.createConnection({
-  host     : '192.168.0.11',
+  host     : '66.96.231.241',
   user     : 'toko',
   password : 'test',
   database : 'toko_12166218'
 });
-
-  exp.get('/rows', (req, res) => {
+var pilih = "nama_user";
+  exp.get('/user', (req, res) => {
   let sql = 'SELECT * FROM user';
-  let query = db.query(sql,(err, results) =>
+  let query = db.query(sql,function(err, results)
   {
+    //untuk pilih key dalam object results
+    var pilih = "nama_user";
       if (err) throw err;
-      res.send(results);
+      //pilih row 1 dari results dimsukin ke variable obj
       var Obj=(results[0]);
-      console.log(Obj);
-      var myJson = JSON.stringify(Obj.nama_user);
+      var myJson = JSON.stringify(Obj);
+
       console.log(myJson);
-      // storage.set('nama_user',myJson,function(error){
-      //   if(error) throw error;
-      //
+
+      // document.getElementById("table").innerHTML = text.nama_user;
+      // console.log(text[pilih]);
+      storage.set('nama_user',myJson,function(error){
+      if(error) throw error;
+    });
       // storage.get('nama_user',function(error,data){
       //   if(error) throw error;
-      //   console.log(data.nama_user);
+      //   console.log(data);
       // });
       // });
       // localStorage,setItem("testJSON",myJson);
@@ -36,7 +117,42 @@ var db = mysql.createConnection({
 exp.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
-// text = localStorage.getItem("testJSON");
-// myText = JSON.parse(text);
-// console.log(myText);
-// document.getElementById("table").innerHTML = myText.nama_user;
+
+
+
+
+// Create menu template
+const mainMenuTemplate = [
+{
+  label:'file',
+  submenu: [
+    {
+      label: 'Quit',
+      accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+      click(item,focusedWindow){
+        focusedWindow.close();
+        }
+      }
+
+  ]
+}
+];
+
+// Add developer tools if still on beta
+if(process.env.NODE_ENV !== 'production'){
+  mainMenuTemplate.push({
+    label: 'Developer Tools',
+    submenu: [
+      {
+        label: 'Toggle DevTools',
+        accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+          click(item,focusedWindow){
+          focusedWindow.toggleDevTools();
+        }
+      },
+      {
+        role: 'reload'
+      }
+    ]
+  })
+}
